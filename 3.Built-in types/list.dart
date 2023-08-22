c84bb7fd420f73b9aa4411a985698d74288d37e4
soyunim <togksk123@naver.com>
2023-08-23 00:30:41 +0900

    Dart Study2

---
+++ 3.Built-in types/list.dart
@@ -0,0 +1,20 @@
+void main(){
+    // collection if: 만약 true면 5도 가짐
+  var num = [
+    1,
+    2,
+    3,
+    4,
+    if(true) 5,
+  ];
+
+
+  // collection for: 다른 list를 합칠 수 있고, 합치면서 내용을 약간 변경할 수도 있음.
+  var friends = ['Harry', 'Sirius'];
+  var magician = [
+    'Ronald',
+    'Hermione',
+    for(var friend in friends) "love $friend",
+  ];
+  // magician = ['Ronald', 'Hermione', 'love Harry', 'love Sirius']
+}
